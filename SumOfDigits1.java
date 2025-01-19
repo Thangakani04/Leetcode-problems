16a463db1184e9a04a6c8ab939d5ed6fb53509ce
Thangakani04 <thangakani1113@gmail.com>
2025-01-19 11:53:18 +0530

    Added a print statement

--- SumOfDigits1.java
+++ SumOfDigits1.java
@@ -5,7 +5,7 @@ public class SumOfDigits1 {
 	public static void main(String[] args) {
 		int num=12345;
 		int output = sumOfDigits(num);
-		System.out.println(output);
+		System.out.println("Printing output :" + output);
 		
 	}
 
